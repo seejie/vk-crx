67540f3e44745bdc3affae859f5c89fb85a36019
liusijie <liusijie@vipkid.com.cn>
2019-10-26 16:10:51 +0800

    周报提醒

--- global/background.js
+++ global/background.js
@@ -2,55 +2,73 @@ const chr = chrome
 console.log(chr, '----------')
 const currVersion = chrome.app.getDetails().version
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", 'https://github.com/seejie/vk-crx/blob/master/manifest.json', true);
-xhr.send();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) { 
-    const res = xhr.response
-    const version = /<\/span>(\d+\.\d+)<span class="pl-pds">/.exec(res)[1]
-    if (version > currVersion) {
-      window.open('https://github.com/seejie/vk-crx/archive/master.zip')
+// check update
+const checkUpdate = _ => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", 'https://github.com/seejie/vk-crx/blob/master/manifest.json', true);
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) { 
+      const res = xhr.response
+      const version = /<\/span>(\d+\.\d+)<span class="pl-pds">/.exec(res)[1]
+      if (version > currVersion) {
+        window.open('https://github.com/seejie/vk-crx/archive/master.zip')
+      }
     }
   }
 }
 
-
-
-const time2WriteReport = _ => {
-  chrome.notifications.create(null, {
-    type: 'basic',
-    iconUrl: 'images/logo.png',
-    title: 'ヾ(◍°∇°◍)ﾉﾞ',
-    message: '工作辛苦了，今天是周五，记得写周报哦！',
-    buttons: [{
-      title: '30分钟后再提示'
-    },{
-      title: '60分钟后再提示'
-    }]
-  })
-  chrome.notifications.onClicked.addListener(_ => {
-    window.open('http://wiki.vipkid.com.cn/pages/viewpage.action?pageId=81141120')
-  })
-  chrome.notifications.onButtonClicked.addListener((id, index) => {
-    const time = index ? 60 : 30
-    const timer = setTimeout(_ => {
+const reportNotify = _ => {
+  let timer
+  const time2WriteReport = _ => {
+    chrome.notifications.create(null, {
+      type: 'basic',
+      iconUrl: 'images/logo.png',
+      title: 'ヾ(◍°∇°◍)ﾉﾞ',
+      message: '工作辛苦了，今天是周五，记得写周报哦！',
+      buttons: [{
+        title: '30分钟后再提示'
+      },{
+        title: '60分钟后再提示'
+      }]
+    })
+    chrome.notifications.onClicked.addListener(_ => {
+      window.open('http://wiki.vipkid.com.cn/pages/viewpage.action?pageId=81141120')
+    })
+    chrome.notifications.onButtonClicked.addListener((id, index) => {
+      const time = index ? 60 : 30
+      clearTimeout(timer)
+      timer = setTimeout(_ => {
+        time2WriteReport()
+      }, time * 60 * 1000)
+    })
+    chrome.notifications.onClosed.addListener(_ => {
+      clearTimeout(timer)
+    })
+  }
+  
+  const friday = 5
+  if (new Date().getDay() === friday) {
+    const clock = new Date()
+    clock.setHours(15)
+    clock.setMinutes(0)
+    clock.setSeconds(0)
+    clock.setMilliseconds(0)
+    
+    const delay = clock.getTime() - new Date().getTime()
+    if (delay < 0) return
+    timer = setTimeout(_ => {
       time2WriteReport()
       clearTimeout(timer)
-    }, time * 60 * 1000)
-  })
+    }, delay)
+  }
 }
 
-const friday = 5
-if (new Date().getDay() === friday) {
-  const clock = new Date()
-  clock.setHours(17)
-  clock.setMinutes(0)
-  clock.setSeconds(0)
-  clock.setMilliseconds(0)
-  const timer = setTimeout(_ => {
-    time2WriteReport()
-    clearTimeout(timer)
-  }, clock.getTime() - new Date().getTime())
+
+const run = _ => {
+  checkUpdate()
+  reportNotify()
 }
 
+// start form here
+run()
