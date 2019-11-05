b168b4bb550f013ef424320413896e7e633d3fa6
liusijie <liusijie@vipkid.com.cn>
2019-11-05 19:50:18 +0800

    better

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,13 @@
+// notify
+const notify = _ => {
+  chrome.notifications.create(null, {
+    type: 'basic',
+    iconUrl: '../images/logo.png',
+    title: 'ヾ(◍°∇°◍)ﾉﾞ',
+    message: '修改成功，刷新后生效！'
+  })
+}
+
 // all config
 document.addEventListener('DOMContentLoaded', _ => {
   const $report = _qs('#weeklyReport')
@@ -8,13 +18,13 @@ document.addEventListener('DOMContentLoaded', _ => {
     _setConfig({allowWeeklyReport: $report.checked})
     _setConfig({allowGitlab: $gitlab.checked})
     _setConfig({allowNewtab: $newtab.checked})
-    alert('修改成功！刷新后生效！')
+    notify()
   }
 
   chrome.storage.local.get(null, storage => {
-    console.log(storage, '----------')
     $report.checked = storage.allowWeeklyReport === undefined ? true : storage.allowWeeklyReport
     $gitlab.checked = storage.allowGitlab === undefined ? true : storage.allowGitlab
     $newtab.checked = storage.allowNewtab === undefined ? true : storage.allowNewtab
   })
 })
+
